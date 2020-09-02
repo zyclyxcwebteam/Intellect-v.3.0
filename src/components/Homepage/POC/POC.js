7ed50f72c9a60785411d5bb7eb36a2b980071ad0
skanjarla <skanjarla@zyclyx.com>
2020-09-02 09:40:33 +0530

    Update POC style
    - move poc code to separate component
    - update bg color

---
+++ src/components/Homepage/POC/POC.js
@@ -0,0 +1,61 @@
+// POC - Proof of Concept section
+
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import "./POC.css";
+
+const POC = () => {
+  return (
+    <Container fluid className="py-5 poc-wrapper">
+      <Container>
+        <h2 className="text-md-center text-left poc-title">
+          <small>Fostering your trust in us with</small>
+          <br />
+          Free Proof of Concept
+        </h2>
+        <p className="text-justify py-md-4 py-2">
+          We believe in developing a trusting partnership with our clients by
+          offering free proof of concept as part of our value added services.
+          With this we demonstrate our work potential by creating an advanced
+          prototype of the project. This allows you to checkout its feasibility
+          in your business process, hence optimizing the experience.
+        </p>
+
+        <Row className="py-3">
+          <Col sm="12" md="4">
+            <Row>
+              <Col sm="1" />
+              <Col sm="11" className="d-flex flex-column">
+                <h3 className="poc-card-title">Forecast</h3>
+                <p>
+                  Act as a tool to forecast the feasibility of the projects.
+                </p>
+              </Col>
+            </Row>
+          </Col>
+
+          <Col sm="12" md="4">
+            <Row>
+              <Col sm="1" />
+              <Col sm="11" className="d-flex flex-column">
+                <h3 className="poc-card-title">Report</h3>
+                <p>Provides a complete report on project evaluation</p>
+              </Col>
+            </Row>
+          </Col>
+
+          <Col sm="12" md="4">
+            <Row>
+              <Col sm="1" />
+              <Col sm="11" className="d-flex flex-column">
+                <h3 className="poc-card-title">Cost Effective</h3>
+                <p>Saves your businesses time and money</p>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+};
+export default POC;
