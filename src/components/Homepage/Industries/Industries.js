524ef599bdf9570c5b450ebfa4ac2e36e7e9d950
skanjarla <skanjarla@zyclyx.com>
2020-09-04 16:55:18 +0530

    Add Images

--- src/components/Homepage/Industries/Industries.js
+++ src/components/Homepage/Industries/Industries.js
@@ -1,36 +1,40 @@
 import React, { useState } from "react";
 import { Container, Row, Col } from "reactstrap";
-import bankImg from "../../../../static/images/bank_test.svg";
+import bankImg from "../../../../static/images/industries/bank.svg";
+import retailImg from "../../../../static/images/industries/retail.svg";
+import telecomImg from "../../../../static/images/industries/telecom.svg";
+import healthImg from "../../../../static/images/industries/health-care.svg";
+import logisticsImg from "../../../../static/images/industries/logistics.svg";
 import "./Industries.css";
 
 const IndustryData = [
   {
-    active: true,
+    img: bankImg,
     title: "Banking Industry",
     text:
       "With our handy services related to banking and financial software, we provide complete solutions for business that aspire to increase the level of operational efficiency. Our solutions for banking and financial include Artificial Intelligence, Internet of Things, Blockchain, AR/VR and Bigdata Analytics applications",
   },
   {
-    active: false,
+    img: telecomImg,
     title: "Telecom Industry",
     text:
       "Telecom industry has been successfully drawing in more revenue with the help of numerous operations. Our team is providing our clients with an end-to-end offshore software solution that would help them to enhance their productivity by working on stable platforms that offers mobility, social media as well as cloud computing solutions.",
   },
 
   {
-    active: false,
+    img: retailImg,
     title: "Retail Industry",
     text:
       "We offer solutions for multiple levels of operational methodologies continuing to grow with every customer. We help you actualize the retail software services and offer you first rated benefits such as direct operations management, enhancement in accurate workflow, better customer care, continuous data flow, better customer care, real time communication",
   },
   {
-    active: false,
+    img: healthImg,
     title: "HealthCare Industry",
     text:
       "Being one of the fastest growing industries, the medicinal sectors need to deal with numerous advancements as well as modern techniques. We provide healthcare sectors with a task-driven solution that would help them improve their accuracy and enhance their medicinal workflow. We build wide range of scalable, cost effective and robust application development",
   },
   {
-    active: false,
+    img: logisticsImg,
     title: "Logistics Industry",
     text:
       "Our team understands the constant pressure businesses have to reduce overhead. We offer you valuable automation solutions like Smart Documentation, Shipment Tracking, Inventory Control, Order Processing, Shipment status communication This ensures your employees can dedicate their efforts to more important tasks.",
@@ -42,6 +46,7 @@ const Industries = () => {
     index: 0,
     title: IndustryData[0].title,
     text: IndustryData[0].text,
+    img: IndustryData[0].img,
   });
 
   const updateIndustryText = index => {
@@ -49,6 +54,7 @@ const Industries = () => {
       index,
       title: IndustryData[index].title,
       text: IndustryData[index].text,
+      img: IndustryData[index].img,
     });
   };
 
@@ -68,7 +74,7 @@ const Industries = () => {
           <h2 className="text-center py-4 ind-section-title">
             INDUSTRY EXPERTISE
           </h2>
-          <img src={bankImg} alt="telecom industry" />
+          <img src={indContent.img} alt="telecom industry" />
         </Col>
         <Col sm="12" md="7" className="right-block">
           <div className="p-5">
