e6589bdd6432a71449f650e7ea7bf7f6ffdd5887
skanjarla <skanjarla@zyclyx.com>
2020-09-07 18:07:45 +0530

    Update images

--- src/components/Homepage/CoreServices/CoreServices.js
+++ src/components/Homepage/CoreServices/CoreServices.js
@@ -10,17 +10,14 @@ import {
 } from "reactstrap";
 import "./CoreServices.css";
 import SectionTitle from "../../SectionTitle/SectionTitle";
-// import NewAgeBG from "../../../../static/images/services/new-age.jpg";
-// import NetworkBG from "../../../../static/images/services/security.jpg";
-// import RPABG from "../../../../static/images/services/rpa.jpg";
-// import OCRBG from "../../../../static/images/services/ocr.jpg";
-// import NewAgeBG from "../../../../static/images/service-pages/2.png";
-// import NetworkBG from "../../../../static/images/service-pages/19.png";
-// import RPABG from "../../../../static/images/service-pages/10.png";
-// import OCRBG from "../../../../static/images/service-pages/3.png";
+import NewAgeBG from "../../../../static/images/services/4.png";
+import NetworkBG from "../../../../static/images/services/3.png";
+import RPABG from "../../../../static/images/services/2.png";
+import OCRBG from "../../../../static/images/services/1.png";
 
 const items = [
   {
+    img: NewAgeBG,
     serviceText:
       "Artificial intelligence is a branch that allows creating smart machines based on the principle of human intelligence. Our team of AI experts works on machines to help them mimic and execute tasks, from the simplest to complex. It aims to include learning, reasoning, and perception.",
     captionOne: "Embrace the future with",
@@ -30,6 +27,7 @@ const items = [
     linkTo: "/artificial-intelligence",
   },
   {
+    img: RPABG,
     serviceText:
       "Robotic Process Automation automates repetitive human tasks by developing an interface with current IT systems without any complex integrations. We assist you to minimize operating costs and optimize performance by innovating, inventing and automating high-quality business processes.",
     captionOne: "Transform your business with",
@@ -39,6 +37,7 @@ const items = [
     linkTo: "/robotic-process-automation",
   },
   {
+    img: OCRBG,
     serviceText:
       "The OCR technology converts virtually any kind of documents, such as scanned paper documents, PDF files, or images captured by a digital camera into machine-readable text data which is editable and searchable. It saves you a lot of time and effort when creating, processing, and repurposing various documents.",
     captionOne: "Adopt smart solutions with",
@@ -48,6 +47,7 @@ const items = [
     linkTo: "/optical-character-recognition",
   },
   {
+    img: NetworkBG,
     serviceText:
       "When you have to work in a world of devious cybercriminals, it’s important to ensure that your network and data are safe. We at ZYCLYX use a combination of effective hardware and software solutions to protect you from multiple layers of intrusions and breaches.",
     captionOne: "Effective Data protection with",
@@ -90,7 +90,7 @@ const OurServices = () => {
         <Row>
           {/* {item.imgLeft && ( */}
           <Col md={6} sm={12} className="service-img d-md-block d-none">
-            {/* <img src={item.src} alt="new age" /> */}
+            <img src={item.img} alt="new age" />
           </Col>
           {/* )} */}
           <Col md={6} sm={12} className="">
