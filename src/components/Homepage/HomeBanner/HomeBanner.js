12e0f49e119047b1efdceb3619406f6a9637c539
skanjarla <skanjarla@zyclyx.com>
2020-09-07 20:24:29 +0530

    Update home page banner images

--- src/components/Homepage/HomeBanner/HomeBanner.js
+++ src/components/Homepage/HomeBanner/HomeBanner.js
@@ -1,8 +1,17 @@
 import React, { useState } from "react";
-import Particles from "react-particles-js";
-import { Carousel, CarouselItem, CarouselIndicators } from "reactstrap";
+// import Particles from "react-particles-js";
+import {
+  Carousel,
+  CarouselItem,
+  CarouselIndicators,
+  Row,
+  Col,
+} from "reactstrap";
 // import img from "../../../../static/images/tech.svg";
 import "./HomeBanner.css";
+import Automation from "../../../../static/images/home-banner/manufacturing.svg";
+import Fuel from "../../../../static/images/home-banner/project.svg";
+import Solution from "../../../../static/images/home-banner/innovation.svg";
 
 // data.allFile.edges.node.childImageSharp.fluid
 const ImageCarousel = () => {
@@ -11,18 +20,21 @@ const ImageCarousel = () => {
 
   const captions = [
     {
+      img: Automation,
       altText: "Slide 1",
       caption1: "Optimizing Business performance",
       caption2: "with Intelligent Automation",
       index: 1,
     },
     {
+      img: Solution,
       altText: "Slide 2",
       caption1: "Moving forward",
       caption2: "with innovative solutions",
       index: 2,
     },
     {
+      img: Fuel,
       altText: "Slide 3",
       caption1: "Fueling change with",
       caption2: "transformative technology",
@@ -55,183 +67,22 @@ const ImageCarousel = () => {
         key={item.index}
         className={`hero-slide slide-${item.index}`}
       >
-        <Particles
-          params={{
-            detectRetina: false,
-            interactivity: {
-              detectsOn: "canvas",
-              events: {
-                onClick: {
-                  enable: false,
-                  mode: "push",
-                },
-                onDiv: {
-                  elementId: "repulse-div",
-                  enable: false,
-                  mode: "repulse",
-                },
-                onHover: {
-                  enable: true,
-                  mode: "bubble",
-                  parallax: {
-                    enable: false,
-                    force: 2,
-                    smooth: 10,
-                  },
-                },
-                resize: true,
-              },
-              modes: {
-                bubble: {
-                  distance: 100,
-                  duration: 2,
-                  opacity: 8,
-                  size: 10,
-                  speed: 3,
-                },
-                connect: {
-                  distance: 120,
-                  lineLinked: {
-                    opacity: 0.8,
-                  },
-                  radius: 60,
-                },
-                grab: {
-                  distance: 400,
-                  lineLinked: {
-                    opacity: 1,
-                  },
-                },
-                push: {
-                  quantity: 4,
-                },
-                remove: {
-                  quantity: 2,
-                },
-                repulse: {
-                  distance: 200,
-                  duration: 0.4,
-                },
-                slow: {
-                  active: false,
-                  radius: 0,
-                  factor: 1,
-                },
-              },
-            },
-            particles: {
-              color: {
-                value: ["#4285f4", "#34A853", "#FBBC05", "#EA4335"],
-              },
-              lineLinked: {
-                blink: true,
-                color: "random",
-                consent: false,
-                distance: 40,
-                enable: true,
-                opacity: 0.8,
-                width: 2,
-              },
-              move: {
-                attract: {
-                  enable: false,
-                  rotate: {
-                    x: 600,
-                    y: 1200,
-                  },
-                },
-                bounce: false,
-                direction: "none",
-                enable: true,
-                outMode: "bounce",
-                random: false,
-                speed: 1,
-                straight: false,
-              },
-              number: {
-                density: {
-                  enable: false,
-                  area: 2500,
-                },
-                limit: 0,
-                value: 250,
-              },
-              opacity: {
-                animation: {
-                  enable: true,
-                  minimumValue: 0.3,
-                  speed: 3,
-                  sync: false,
-                },
-                random: false,
-                value: 0.8,
-              },
-              shape: {
-                character: {
-                  fill: false,
-                  font: "Verdana",
-                  style: "",
-                  value: "*",
-                  weight: "400",
-                },
-                image: {
-                  height: 400,
-                  replaceColor: true,
-                  //   src: "https://cdn.matteobruni.it/images/particles/github.svg",
-                  width: 400,
-                },
-                polygon: {
-                  sides: 5,
-                },
-                stroke: {
-                  color: "#000000",
-                  width: 0,
-                },
-                type: "circle",
-              },
-              size: {
-                animation: {
-                  enable: false,
-                  minimumValue: 0.1,
-                  speed: 20,
-                  sync: false,
-                },
-                random: true,
-                value: 6,
-              },
-            },
-            polygon: {
-              draw: {
-                enable: false,
-                lineColor: "rgba(255,255,255,0.2)",
-                lineWidth: 1,
-              },
-              enable: true,
-              move: {
-                radius: 5,
-              },
-              position: {
-                x: 30,
-                y: 10,
-              },
-              inlineArrangement: "equidistant",
-              scale: 1,
-              type: "inline",
-              url: "https://cdn.matteobruni.it/images/particles/google.svg",
-            },
-            background: {
-              image: "",
-              position: "0% 50%",
-              repeat: "no-repeat",
-              size: "cover",
-            },
-          }}
-        />
-        <h1 className="home-banner-title">
-          <span>{item.caption1}</span>
-          <br />
-          <span>{item.caption2}</span>
-        </h1>
+        <Row className="h-100">
+          <Col sm="12" md="7">
+            <h1 className="home-banner-title">
+              <span>{item.caption1}</span>
+              <br />
+              <span>{item.caption2}</span>
+            </h1>
+          </Col>
+          <Col
+            sm="12"
+            md="5"
+            className="d-flex justify-content-center align-items-center"
+          >
+            <img src={item.img} alt="automation" />
+          </Col>
+        </Row>
       </CarouselItem>
     );
   });
@@ -241,7 +92,7 @@ const ImageCarousel = () => {
       activeIndex={activeIndex}
       next={nextSlide}
       previous={previousSlide}
-      className="banner-carousel carousel-fade"
+      className="banner-carousel"
     >
       <CarouselIndicators
         items={captions}
