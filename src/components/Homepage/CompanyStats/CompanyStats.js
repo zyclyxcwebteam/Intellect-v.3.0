62f6cbce017b75d93cba8552da6e89bd8c296d2a
skanjarla <skanjarla@zyclyx.com>
2020-09-08 19:13:33 +0530

    Update company stats section design

--- src/components/Homepage/CompanyStats/CompanyStats.js
+++ src/components/Homepage/CompanyStats/CompanyStats.js
@@ -1,66 +1,51 @@
 import React from "react";
 import { Container, Row, Col } from "reactstrap";
-import RocketGIF from "../../../../static/images/rocket.gif";
-import SettingsGIF from "../../../../static/images/settings.gif";
-import UserGIF from "../../../../static/images/client.gif";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./CompanyStats.css";
+
+const StatsCard = props => {
+  const { icon, animation, number, text } = props;
+  return (
+    <Col sm={12} md={4}>
+      <div className="d-flex flex-column justify-content-center align-items-center shadow shadow-lg p-4 rounded-lg stats">
+        <div className="d-flex align-items-center justify-content-center">
+          <FontAwesomeIcon
+            icon={icon}
+            className={`mr-4 stat-icon ${animation}`}
+          />
+          <div className="d-flex flex-column py-3">
+            <h3 className="stat-title text-center">{`${number}+`}</h3>
+          </div>
+        </div>
+        <p className="stat-caption text-center mb-0 py-3">{text}</p>
+      </div>
+    </Col>
+  );
+};
 
 const CompanyStats = () => {
   return (
     <Container fluid className="stats-wrapper py-2 py-md-5">
       <Container className=" py-2 py-md-5">
         <Row>
-          <Col sm={12} md={4}>
-            <div className="d-flex flex-column justify-content-center align-items-center shadow shadow-lg p-4 rounded-lg stats">
-              <div className="d-flex align-items-center justify-content-center">
-                <div className="m-4">
-                  <img
-                    src={UserGIF}
-                    alt="clients"
-                    className="stats-icon client-icon"
-                  />
-                </div>
-                <div className="d-flex flex-column">
-                  <h3 className="stat-title text-center">20+</h3>
-                </div>
-              </div>
-              <p className="stat-caption text-center">Clients Served</p>
-            </div>
-          </Col>
-          <Col sm={12} md={4}>
-            <div className="d-flex flex-column justify-content-center align-items-center shadow shadow-lg p-4 rounded-lg stats">
-              <div className="d-flex justify-content-center align-items-center">
-                <div className="m-4">
-                  <img
-                    src={RocketGIF}
-                    alt="clients"
-                    className="stats-icon project-icon"
-                  />
-                </div>
-                <div className="d-flex flex-column">
-                  <h3 className="stat-title text-center">40+</h3>
-                </div>
-              </div>
-              <p className="stat-caption text-center">Projects Delivered</p>
-            </div>
-          </Col>
-          <Col sm={3} md={4}>
-            <div className="d-flex flex-column justify-content-center align-items-center shadow shadow-lg p-4 rounded-lg stats">
-              <div className="d-flex justify-content-center align-items-center">
-                <div className="m-4">
-                  <img
-                    src={SettingsGIF}
-                    alt="clients"
-                    className="stats-icon process-icon"
-                  />
-                </div>
-                <div className="">
-                  <h3 className="stat-title text-center">350+</h3>
-                </div>
-              </div>
-              <p className="stat-caption text-center">Processess Automated</p>
-            </div>
-          </Col>
+          <StatsCard
+            text="Clients Served"
+            icon="users"
+            number="20"
+            animation="fa-pulse"
+          />
+          <StatsCard
+            text="Projects Delivered"
+            icon="rocket"
+            number="40"
+            animation="fa-rising"
+          />
+          <StatsCard
+            text="Processess Automated"
+            icon="cog"
+            number="350"
+            animation="fa-spin"
+          />
         </Row>
       </Container>
     </Container>
