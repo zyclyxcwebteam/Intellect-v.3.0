7535488ea897e671881b990cf48a0af1ac271844
skanjarla <skanjarla@zyclyx.com>
2020-09-09 10:42:15 +0530

    Update title case

--- src/components/Homepage/HomeBanner/HomeBanner.js
+++ src/components/Homepage/HomeBanner/HomeBanner.js
@@ -22,22 +22,22 @@ const ImageCarousel = () => {
     {
       img: Fuel,
       altText: "Slide 3",
-      caption1: "Fueling change with",
-      caption2: "transformative technology",
+      caption1: "Fueling Change with",
+      caption2: "Transformative Technology",
       index: 1,
     },
     {
       img: Automation,
       altText: "Slide 1",
-      caption1: "Optimizing Business performance",
+      caption1: "Optimizing Business Performance",
       caption2: "with Intelligent Automation",
       index: 2,
     },
     {
       img: Solution,
       altText: "Slide 2",
-      caption1: "Moving forward",
-      caption2: "with innovative solutions",
+      caption1: "Moving Forward",
+      caption2: "with Innovative Solutions",
       index: 3,
     },
   ];
@@ -68,7 +68,7 @@ const ImageCarousel = () => {
         className={`hero-slide slide-${item.index}`}
       >
         <Row className="h-100">
-          <Col sm="12" md="7">
+          <Col sm="12" md="7" className="title-container">
             <h1 className="home-banner-title">
               <span>{item.caption1}</span>
               <br />
