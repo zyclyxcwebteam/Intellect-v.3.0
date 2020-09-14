c19c35c8b19811bf873d78f44e8ede9d7256a865
skanjarla <skanjarla@zyclyx.com>
2020-09-14 18:57:56 +0530

    Update images

--- src/components/Homepage/Industries/Industries.js
+++ src/components/Homepage/Industries/Industries.js
@@ -5,6 +5,7 @@ import retailImg from "../../../../static/images/industries/RETAIL2.png";
 import telecomImg from "../../../../static/images/industries/telecom.svg";
 import healthImg from "../../../../static/images/industries/h.svg";
 import logisticsImg from "../../../../static/images/industries/logistics.svg";
+import hospitalityImg from "../../../../static/images/industries/hospitality.svg";
 import "./Industries.css";
 
 const IndustryData = [
@@ -22,10 +23,10 @@ const IndustryData = [
   },
 
   {
-    img: retailImg,
-    title: "Retail Industry",
+    img: hospitalityImg,
+    title: "Hospitality Industry",
     text:
-      "Our retail offering is aimed at multiple levels of operational methodologies to grow with every customer. We help you actualize the retail software services and offer you benefits such as direct operations management, improved workflow, better customer care, continuous data flow, and real-time communication.",
+      "Our immense expertise in the hospitality industry ensures that we contribute to improving the performance and satisfaction of our clients. We work across various platforms like Point-of-Sale (POS) apps, CRM systems, Infrastructure services, enterprise systems, and more. These aim to transform your business operations to provide excellent customer experiences.",
   },
   {
     img: healthImg,
@@ -38,6 +39,12 @@ const IndustryData = [
     title: "Logistics Industry",
     text:
       "Our team understands the constant pressure businesses have to reduce overhead. We offer you valuable automation solutions like Smart Documentation, Shipment Tracking, Inventory Control, Order Processing, Shipment status communication This ensures your employees can dedicate their efforts to more important tasks.",
+  },
+  {
+    img: retailImg,
+    title: "Retail Industry",
+    text:
+      "Our retail offering is aimed at multiple levels of operational methodologies to grow with every customer. We help you actualize the retail software services and offer you benefits such as direct operations management, improved workflow, better customer care, continuous data flow, and real-time communication.",
   },
 ];
 
@@ -70,7 +77,7 @@ const Industries = () => {
   return (
     <Container fluid className="ind-wrapper">
       <Row className="h-100">
-        <Col sm="12" md="5" className="left-block">
+        <Col sm="12" md="4" className="left-block">
           <h2 className="text-center py-4 ind-section-title">
             INDUSTRY EXPERTISE
           </h2>
@@ -78,7 +85,7 @@ const Industries = () => {
             <img src={indContent.img} alt="telecom industry" />
           </div>
         </Col>
-        <Col sm="12" md="7" className="right-block">
+        <Col sm="12" md="8" className="right-block">
           <div className="p-md-5 p-2">
             <h3 className="py-md-5  py-2 ind-title">{indContent.title}</h3>
             <p className="text-justify ind-text">{indContent.text}</p>
@@ -91,7 +98,7 @@ const Industries = () => {
                 }`}
                 onMouseEnter={() => updateIndustryText(0)}
               >
-                <h4 className="font-weight-bold d-none d-md-block">Banking</h4>
+                <h4 className="d-none d-md-block">Banking</h4>
                 <h4 className="font-weight-bold d-md-none d-block">B</h4>
               </div>
             </Col>
@@ -102,7 +109,7 @@ const Industries = () => {
                 }`}
                 onMouseEnter={() => updateIndustryText(1)}
               >
-                <h4 className="font-weight-bold d-none d-md-block">Telecom</h4>
+                <h4 className="d-none d-md-block">Telecom</h4>
                 <h4 className="font-weight-bold d-block d-md-none">T</h4>
               </div>
             </Col>
@@ -113,8 +120,8 @@ const Industries = () => {
                 }`}
                 onMouseEnter={() => updateIndustryText(2)}
               >
-                <h4 className="font-weight-bold d-none d-md-block">Retail</h4>
-                <h4 className="font-weight-bold d-md-none d-block">R</h4>
+                <h4 className="d-none d-md-block">Hospitality</h4>
+                <h4 className="font-weight-bold d-md-none d-block">H</h4>
               </div>
             </Col>
             <Col className="px-md-3 px-1">
@@ -124,9 +131,7 @@ const Industries = () => {
                 }`}
                 onMouseEnter={() => updateIndustryText(3)}
               >
-                <h4 className="font-weight-bold d-none d-md-block">
-                  Healthcare
-                </h4>
+                <h4 className="d-none d-md-block">Healthcare</h4>
                 <h4 className="font-weight-bold d-md-none d-block">H</h4>
               </div>
             </Col>
@@ -137,10 +142,19 @@ const Industries = () => {
                 }`}
                 onMouseEnter={() => updateIndustryText(4)}
               >
-                <h4 className="font-weight-bold d-none d-md-block">
-                  Logistics
-                </h4>
+                <h4 className="d-none d-md-block">Logistics</h4>
                 <h4 className="font-weight-bold d-md-none d-block">L</h4>
+              </div>
+            </Col>
+            <Col className="px-md-3 px-1">
+              <div
+                className={`shadow shadow-sm rounded-lg text-center ind-menu-card ${
+                  indContent.index === 5 && "active"
+                }`}
+                onMouseEnter={() => updateIndustryText(5)}
+              >
+                <h4 className="d-none d-md-block">Retail</h4>
+                <h4 className="font-weight-bold d-md-none d-block">R</h4>
               </div>
             </Col>
           </Row>
