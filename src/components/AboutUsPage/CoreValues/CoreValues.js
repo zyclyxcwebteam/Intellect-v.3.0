d0397eaaa6165799945518eba64a94e4355888db
skanjarla <skanjarla@zyclyx.com>
2020-09-14 18:05:48 +0530

    Update core values section design

---
+++ src/components/AboutUsPage/CoreValues/CoreValues.js
@@ -0,0 +1,104 @@
+import React from "react";
+import { Container } from "reactstrap";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import IconDeliver from "../../../../static/images/c-deliver.svg";
+import IconOwnership from "../../../../static/images/c-val-2.svg";
+import IconCommit from "../../../../static/images/c-val-3.svg";
+import IconBuild from "../../../../static/images/c-val-4.svg";
+import IconCreative from "../../../../static/images/c-val-5.svg";
+import IconCount from "../../../../static/images/c-val-6.svg";
+
+import "./CoreValues.css";
+
+const settings = {
+  dots: true,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        initialSlide: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+};
+const CoreValueCard = ({ title, text, icon }) => {
+  return (
+    <div className="card m-4 p-2">
+      <img className="card-img-top p-5 " src={icon} alt="Card cap" />
+      <div className="card-body">
+        <h4 className="card-title text-center">{title}</h4>
+        <p className="card-text text-center">{text}</p>
+      </div>
+    </div>
+  );
+};
+
+const CoreValues = () => {
+  return (
+    <Container fluid className="py-5 core-values">
+      <h2 className="text-center text-white core-section-title">Core Values</h2>
+      <Container className="py-3">
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        <Slider {...settings}>
+          <CoreValueCard
+            title="Deliver"
+            text="amazing service"
+            icon={IconDeliver}
+          />
+          <CoreValueCard
+            title="Commit"
+            text="to evolve"
+            icon={IconCommit}
+            left
+          />
+          <CoreValueCard
+            title="Embrace Creativity"
+            text="and innovation"
+            icon={IconCreative}
+          />
+          <CoreValueCard
+            title="Take Ownership"
+            text="and drive change"
+            icon={IconOwnership}
+          />
+
+          <CoreValueCard
+            title="Build"
+            text="Positive Environment"
+            icon={IconBuild}
+          />
+          <CoreValueCard title="Make it" text="Count" icon={IconCount} />
+        </Slider>
+      </Container>
+    </Container>
+  );
+};
+
+export default CoreValues;
--- src/pages/aboutus.js
+++ src/pages/aboutus.js
@@ -1,48 +1,12 @@
 import React from "react";
 import { Container, Row, Col } from "reactstrap";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import RotatingText from "react-rotating-text";
 import Layout from "../components/Layout/Layout";
 import SectionTitle from "../components/SectionTitle/SectionTitle";
 import MissionAndVision from "../components/AboutUsPage/MissionAndVision/MissionAndVision";
-// import MissionImage from "../../static/images/mission.png";
-// import VisionImage from "../../static/images/vision.png";
-import IconDeliver from "../../static/images/c-deliver.svg";
-import IconOwnership from "../../static/images/c-val-2.svg";
-import IconCommit from "../../static/images/c-val-3.svg";
-import IconBuild from "../../static/images/c-val-4.svg";
-import IconCreative from "../../static/images/c-val-5.svg";
-import IconCount from "../../static/images/c-val-6.svg";
-import "../css/aboutus.css";
+import CoreValues from "../components/AboutUsPage/CoreValues/CoreValues";
 
-// Core Values card
-const CoreValueCard = ({ title, text, icon, left }) => {
-  return left ? (
-    <Row className="c-card">
-      <Col className="col-8 d-flex flex-column justify-content-center align-items-sm-center px-0">
-        <h3 className="c-title text-right">{title}</h3>
-        <p className="c-text text-right mb-0">{text}</p>
-      </Col>
-      <Col className="col-4 d-flex justify-content-center align-items-center">
-        <div className="c-icon">
-          <img src={icon} alt={title} />
-        </div>
-      </Col>
-    </Row>
-  ) : (
-    <Row className="c-card">
-      <Col className="col-4 d-flex justify-content-center align-items-center">
-        <div className="c-icon">
-          <img src={icon} alt={title} />
-        </div>
-      </Col>
-      <Col className="col-8 d-flex flex-column justify-content-center px-0">
-        <h3 className="c-title text-left">{title}</h3>
-        <p className="c-text mb-0 text-left">{text}</p>
-      </Col>
-    </Row>
-  );
-};
+import "../css/aboutus.css";
 
 const aboutus = () => {
   // const [showMission, setShowMission] = useState(true);
@@ -112,119 +76,8 @@ const aboutus = () => {
       </Container>
       <MissionAndVision />
 
-      {/* Mission and Vission */}
-      {/* <Container fluid className="mission-outer py-md-4 py-2 px-md-3 px-0">
-        <Container className="py-md-5 py-3 mission-inner">
-          <Row>
-            <Col
-              sm="12"
-              md="8"
-              className="d-flex align-items-center flex-column px-md-5 px-2"
-            >
-              <h2 className="mission-title">
-                {showMission ? "Mission" : "Vision"}
-              </h2>
-              <p className="px-md-5 px-3 py-md-3 py-2 text-center mission-text">
-                {showMission
-                  ? `To offer custom solutions to Clients using unique combination of
-                data science, amazing design philosophy, software engineering,
-                and architecture.`
-                  : `To provide diverse solutions in the area of Futuristic IT development with a strong commitment towards corporate citizenship and sustainability.
-
-                  `}
-              </p>
-              <div className="mission-links d-flex justify-content-end w-100">
-                <button
-                  type="button"
-                  className={`m-btn d-flex align-items-center ${
-                    !showMission && "m-btn-active"
-                  }`}
-                  onClick={() => setShowMission(!showMission)}
-                >
-                  <FontAwesomeIcon icon="arrow-left" className="mr-2" />
-                  Mission
-                </button>
-                <button
-                  type="button"
-                  className={`m-btn d-flex align-items-center ${
-                    showMission && "m-btn-active"
-                  }`}
-                  onClick={() => setShowMission(!showMission)}
-                >
-                  Vision
-                  <FontAwesomeIcon icon="arrow-right" className="ml-2" />
-                </button>
-              </div>
-            </Col>
-            <Col
-              sm="12"
-              md="4"
-              className="justify-content-center d-none d-md-flex"
-            >
-              <img
-                src={showMission ? MissionImage : VisionImage}
-                alt="mission"
-                className="m-img"
-              />
-            </Col>
-          </Row>
-        </Container>
-      </Container> */}
-
       {/* Core Values */}
-      <Container fluid className="py-3 core-values">
-        <Container className="py-3">
-          <Row>
-            <Col sm={12} md={4} className="order-2 order-md-1 px-md-3 px-0">
-              <CoreValueCard
-                title="Deliver"
-                text="amazing service"
-                icon={IconDeliver}
-                left
-              />
-              <CoreValueCard
-                title="Commit"
-                text="to evolve"
-                icon={IconCommit}
-                left
-              />
-              <CoreValueCard
-                title="Embrace Creativity"
-                text="and innovation"
-                icon={IconCreative}
-                left
-              />
-            </Col>
-            <Col
-              sm={12}
-              md={4}
-              className="d-flex justify-content-center align-items-center flex-column order-1 order-md-2"
-            >
-              <h2 className="core-title">Core Values</h2>
-            </Col>
-            <Col sm={12} md={4} className="order-3 px-md-3 px-0">
-              <CoreValueCard
-                title="Take Ownership"
-                text="and drive change"
-                icon={IconOwnership}
-                left={false}
-              />
-              <CoreValueCard
-                title="Build"
-                text="Positive Environment"
-                icon={IconBuild}
-                left={false}
-              />
-              <CoreValueCard
-                title="Make it"
-                text="Count"
-                icon={IconCount}
-                left={false}
-              />
-            </Col>
-          </Row>
-        </Container>
-      </Container>
+      <CoreValues />
       <Container className="py-5">
         <SectionTitle title="How we do it" />
 
