4ce3a3ce46bb34eddc6f5fd37a5908daffe94714
skanjarla <skanjarla@zyclyx.com>
2020-09-18 17:36:17 +0530

    Update mobile responsive

--- src/components/AboutUsPage/MissionAndVision/MissionAndVision.js
+++ src/components/AboutUsPage/MissionAndVision/MissionAndVision.js
@@ -1,17 +1,13 @@
 import React, { useState } from "react";
 import { Container, Row, Col } from "reactstrap";
-// import Mission from "../../../../static/images/mission2.svg";
-// import Vision from "../../../../static/images/focus2.svg";
-// import Mission from "../../../../static/images/aboutus/mission.svg";
-// import Vision from "../../../../static/images/aboutus/vision.svg";
 import "./MissionAndVision.css";
 
 const MissionAndVision = () => {
   const [showMission, setShowMission] = useState(true);
   return (
-    <Container fluid className="py-4">
-      <Container className="mission-wrapper my-5 pl-0">
-        <div className="mission-space" />
+    <Container fluid className="py-4 px-0 px-md-3">
+      <Container className="mission-wrapper my-md-5 pl-0">
+        <div className="mission-space d-none d-md-block" />
         <Row className="h-100">
           <Col
             md="6"
@@ -25,12 +21,12 @@ const MissionAndVision = () => {
             />
             {/* <img src={showMission ? Mission : Vision} alt="Mission" /> */}
           </Col>
-          <Col md="6" sm="12">
-            <div className="mission-text-container py-5 mx-5 px-4">
-              <h2 className="text-center pb-4">
+          <Col md="6" sm="12" className="px-2">
+            <div className="mission-text-container px-2 py-md-5 mx-md-5 px-md-4">
+              <h2 className="text-center pb-2 pb-md-4">
                 {showMission ? "Mission" : "Vision"}
               </h2>
-              <p className="px-5 text-justify">
+              <p className="px-3 px-md-5 text-justify">
                 {showMission
                   ? `To offer custom solutions to Clients using unique combination of data science, amazing design philosophy, software engineering, and architecture.`
                   : `To provide diverse solutions in the area of Futuristic IT development with a strong commitment towards corporate citizenship and sustainability.`}
